162dd36d07016946d952f52eb9b295933ee5a5da
nitishkumar2303 <160706475+nitishkumar2303@users.noreply.github.com>
2024-06-06 19:48:34 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,122 @@
+
+const apikey='2983239c7a014546bb1af5f5ca86401f';
+
+
+const blogContainer = document.getElementById("blog-container");
+
+const searchField = document.getElementById('search-input')
+const searchButton = document.getElementById('search-button')
+
+async function fetchRandomNews(){
+
+    try{
+
+        const apiUrl = `https://newsapi.org/v2/top-headlines?country=us&pageSize=30&apiKey=${apikey}`;
+        const response = await fetch(apiUrl);
+        const data = await response.json();
+        return data.articles;
+
+
+    }catch(error){
+            console.error("Error in fetching the random news", error);
+            return [];
+
+
+    }
+
+
+}
+
+
+searchButton.addEventListener('click' ,async ()=>{
+    const query = searchField.value.trim()
+    if(query!==""){
+        try{
+            const articles = await fetchNewsQuery(query)
+            displayBlogs(articles)
+
+        }catch(error){
+        console.log("Error fetching news by query");
+    }
+}
+
+
+})
+
+async function fetchNewsQuery(query){
+
+    try{
+
+        const apiUrl = `https://newsapi.org/v2/everything?q=${query}&pageSize=30&apiKey=${apikey}`;
+        const response = await fetch(apiUrl);
+        const data = await response.json();
+        return data.articles;
+
+
+    }catch(error){
+            console.error("Error in fetching the random news", error);
+            return [];
+
+
+    }
+
+
+
+}
+
+
+
+function displayBlogs(articles){
+    blogContainer.innerHTML =""
+    articles.forEach((article) =>{
+
+        const blogCard = document.createElement("div")
+        blogCard.classList.add("blog-card")
+        const img = document.createElement("img")
+        if (article.urlToImage === null){
+            img.src = "https://www.thermaxglobal.com/wp-content/uploads/2020/05/image-not-found.jpg"
+        } else {
+            img.src = article.urlToImage
+        }
+        img.alt = article.title
+        const title = document.createElement("h2")
+        const truncatedTitle = article.title.length>30 ? article.title.slice(0,30) + ".."   : article.title   ;
+        title.textContent = truncatedTitle
+        const description = document.createElement("p")
+        const truncatedDescription = article.description && article.description.length> 150 ? article.description.slice(0,120) + "..." :article.description;
+        description.textContent = truncatedDescription
+
+
+        blogCard.appendChild(img);
+        blogCard.appendChild(title);
+        blogCard.appendChild(description);
+        blogCard.addEventListener('click' , ()=> {
+            window.open(article.url , "_blank");
+
+        });
+
+        blogContainer.appendChild(blogCard);
+
+
+    });
+
+
+
+}
+
+((async ()=>{
+    try{
+
+       const articles =  await fetchRandomNews();
+       console.log(articles)
+
+       displayBlogs(articles);
+       
+
+    } catch(error){
+        console.error("Error in fetching the random news", error);
+
+    }
+
+})());
+
